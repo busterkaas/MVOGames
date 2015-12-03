bca53a273ed79eda434be0d0e415c7ccb0ccc295
Buster Kaas Jensen <busterkaas@gmail.com>
2015-12-04 00:00:08 +0100

    User Area: Profile: User information by a Partial view added. UPDATE user information btn - Working!

--- MVOGamesUI/Areas/User/Controllers/ProfileController.cs
+++ MVOGamesUI/Areas/User/Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using MVOGamesUI.Areas.User.ViewModels;
 using MVOGamesUI.Infrastructure;
 using ServiceGateway;
@@ -18,6 +19,7 @@ namespace MVOGamesUI.Areas.User.Controllers
         // GET: User/Profile
         public ActionResult Index()
         {
+            ViewBag.UserUpdated = "";
             var user = facade.GetUserGateway().Get(Auth.user.Id);
             var crews = facade.GetCrewGateway().GetAll().ToList();
             var userCrews = from c in crews
@@ -26,6 +28,26 @@ namespace MVOGamesUI.Areas.User.Controllers
             UserCrew uc = new UserCrew(user, userCrews.ToList());  
                
             return View(uc);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Username,FirstName,LastName,StreetName,HouseNr,ZipCode,City,Email")] ServiceGateway.Models.User user)
+        {
+            ViewBag.UserUpdated = " - User has been updated!";
+            ServiceGateway.Models.User newUser = Auth.user;
+            newUser.Username = user.Username;
+            newUser.FirstName = user.FirstName;
+            newUser.LastName = user.LastName;
+            newUser.StreetName = user.StreetName;
+            newUser.HouseNr = user.HouseNr;
+            newUser.ZipCode = user.ZipCode;
+            newUser.City = user.City;
+            newUser.Email = user.Email;
+            FormsAuthentication.SetAuthCookie(newUser.Username, true);
+            
+            facade.GetUserGateway().Update(newUser);
+            return RedirectToAction("Index");
         }
     }
 }
