c60b7876b105bce2bea964b725c09dbacc8600bc
TasinA <tasin-a@hotmail.com>
2015-12-10 11:59:12 +0100

    Order

--- MVOGamesUI/Areas/Admin/Controllers/OrdersController.cs
+++ MVOGamesUI/Areas/Admin/Controllers/OrdersController.cs
@@ -25,7 +25,7 @@ namespace MVOGamesUI.Areas.Admin.Controllers
 
         // GET: Admin/Order/Details/5
         public ActionResult Details(int? id)
-        {
+        { 
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
