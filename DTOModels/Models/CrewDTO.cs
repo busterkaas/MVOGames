9f193b250ff122ed4376220eb5c17a2d95b2d037
Buster Kaas Jensen <busterkaas@gmail.com>
2015-12-18 15:57:57 +0100

    Model - Annotations

--- DTOModels/Models/CrewDTO.cs
+++ DTOModels/Models/CrewDTO.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +11,13 @@ namespace DTOModels.Models
     public class CrewDTO
     {
         public int Id { get; set; }
-        //[Required]
-        //[MinLength(2)]
-        //[DisplayName("Crew name")]
+        [Required]
+        [MinLength(2)]
+        [DisplayName("Crew name")]
         public string Name { get; set; }
-        //[DisplayName("Crew image url")]
+        [DisplayName("Crew image url")]
         public string CrewImgUrl { get; set; }
         public int CrewLeaderId { get; set; }
-        //public UserDTO CrewLeader { get; set; }
         public List<UserDTO> Users { get; set; }
     }
 }
